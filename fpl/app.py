c0516e7fa6504dba661e01ea5f95475e8561423c
Plavak Das <plavakdas@gmail.com>
2025-01-02 22:31:35 +0530

    basic home page and routing setup

--- fpl/app.py
+++ fpl/app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method=="POST":
+        id = int(request.form.get("id"))
+        return redirect(url_for('login',id=id))        
     return render_template("home.html")
+
+@app.route("/login/<int:id>")
+def login(id):
+    return f"Hello, {id}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
